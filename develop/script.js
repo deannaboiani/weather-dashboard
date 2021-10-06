70264cecee4969b54fcbde134bbf663d587b79a5
DeAnna Boiani <deannaboiani@gmail.com>
2021-10-05 20:57:14 -0700

    current city search shows temp in farenheight, wind speed and humidity in current city container

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,7 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
+// GIVEN a weather dashboard with form inputs √
+// WHEN I search for a city √
+// THEN I am presented with current and future conditions for that city and that city is added to the search history 
+// WHEN I view current weather conditions for that city √
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
@@ -10,28 +10,41 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 console.log('hi')
+// var cardText = document.getElementById('card-text')
 
-$('#locationBtn').click(getApi);
-  // var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=06d6972d9ab98e2a5159408e1850243a"
-  
-  function getApi() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=06d6972d9ab98e2a5159408e1850243a")
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-        console.log(data)
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
+
+function getTemp() {
+  var city = $("#location").val();
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=06d6972d9ab98e2a5159408e1850243a`;
+
+  fetch(requestUrl)
+   .then (function(response) {
+     return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      var temp = document.createElement('p');
+      var wind = document.createElement('p');
+      var humidity = document.createElement('p');
+
+      temp.textContent = data.main.temp;
+      wind.textContent = data.wind.speed;
+      humidity.textContent = data.main.humidity;
+      
+
+      $('#current-city-temp').append(temp);
+      $('#current-city-wind').append(wind);
+      $('#current-city-humidity').append(humidity);
+        
+        
+        
+     
     })
   }
 
 
-  
-  // getApi(requestUrl);
-  
-
-var apiKey = "06d6972d9ab98e2a5159408e1850243a"
-"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+$('#locationBtn').click(function() {
+    getTemp();
+        // console.log(city)
+  })
